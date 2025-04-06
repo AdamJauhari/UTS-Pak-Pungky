e574c57003f9b7a5c11ae6892743a138828362dc
AdamJauhari <adamjauhari12@gmail.com>
2025-04-06 16:59:40 +0700

    Update uts.py

--- uts.py
+++ uts.py
@@ -1,16 +1,14 @@
 import mysql.connector
 import pandas as pd
 
-# Koneksi ke database MySQL
 def create_connection():
     return mysql.connector.connect(
-        host="localhost",      # Alamat MySQL
-        user="root",           # Username MySQL
-        password="",   # Password MySQL
-        database="zakat" # Nama database
+        host="localhost",
+        user="root",
+        password="",   
+        database="zakat" 
     )
 
-# Fungsi untuk menambahkan data zakat
 def add_zakat(nama, jenis_zakat, jumlah, tanggal):
     conn = create_connection()
     cursor = conn.cursor()
